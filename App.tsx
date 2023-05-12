33de1f95fe3fa65c66517b8d4983b316363c790a
prasannas544 <prasanna.swain@stepapp.ai>
2023-05-12 19:05:29 +0530

    notif done

--- App.tsx
+++ App.tsx
@@ -1,21 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import MyStack from './navigation'
+import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import MyStack from './navigation';
+
+import {
+  requestUserPermission,
+  NotificationListner,
+} from './src/utils/pushnotification_helper';
 
 const App = () => {
-  return (
-      <NavigationContainer >
-        <MyStack />
-      </NavigationContainer>
-  )
-}
+  useEffect(() => {
+    requestUserPermission();    
+    NotificationListner();
+  }, []);
 
-export default App
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     // paddingTop: 40,
     paddingHorizontal: 16,
   },
-})
+});
