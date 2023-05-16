642c11204d2a61101d8a0c9fa3aae8eb11a16322
prasannas544 <prasanna.swain@stepapp.ai>
2023-05-16 15:07:41 +0530

    simple card added

--- components/AvatarCard.js
+++ components/AvatarCard.js
@@ -1,4 +1,4 @@
-import { View, Text, ImageBackground, TextInput, TouchableOpacity } from 'react-native'
+import { View, Text, Image, TextInput, TouchableOpacity, StyleSheet } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import { useWindowDimensions } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -37,7 +37,7 @@ const AvatarCard = ({ navigation }) => {
     const windowHeight = useWindowDimensions().height;
 
     useEffect(() => {
-        route()
+        // route()
     }, [])
 
     const route = async () => {
@@ -53,47 +53,124 @@ const AvatarCard = ({ navigation }) => {
         navigation.replace('Home', { userName: user_name });
     }
     return (
-        <View>
-            <ImageBackground source={Avatardata[0].bg} style={{ position: 'relative', padding: 5, borderColor: '#fff', alignSelf: 'center', width: windowWidth * 0.8, height: windowHeight * 0.5 }} >
-                <View style={{ borderColor: '#fff', position: 'absolute', top: 0 }}>
-                    <TextInput
-                        style={{
-                            height: 40,
-                            fontFamily: 'CabinetGrotesk-Bold',
-                            color: '#fff',
-                            borderColor: '#fff'
-                        }}
-                        onChangeText={onChangeText}
-                        value={text}
-                        placeholder="Enter Name Q"
-                        placeholderTextColor='#fff'
-                    />
+        <View style={styles.card}>
+            <View style={styles.content}>
+                <View style={styles.top}>
+                    <Text style={styles.topText}>Orange</Text>
+                    <View style={styles.color}></View>
                 </View>
-                <View style={{ width: '90%', height: '65%', backgroundColor: '#000', alignSelf: 'center', position: 'absolute', top: 30 }}>
-
+                <View style={styles.middle}>
+                    <View>
+                        <Text style={styles.middleText}>#F9BC61</Text>
+                        <Text style={[styles.middleText, {fontSize:16}]}>Charizard</Text>
+                    </View>
+                    <Image source={require('../components/pg1.png')} style={styles.middleImage} resizeMode='contain' />
                 </View>
-                <View style={{ flexDirection: 'column', position: 'absolute', bottom: 40, }}>
-                    <View >
-                        <Text style={{ fontFamily: 'CabinetGrotesk-Bold', fontSize: 16, color: '#fff' }}>IQ:{Avatardata[0].stats.IQ}</Text>
+                <View style={styles.bottom}>
+                    <View style={styles.bottomItem}>
+                        <Text style={styles.bottomText}>Hue</Text>
+                        <Text style={styles.bottomText}>36</Text>
                     </View>
-                    <View >
-                        <Text style={{ fontFamily: 'CabinetGrotesk-Bold', fontSize: 16 }}>Power:{Avatardata[0].stats.power}</Text>
+                    <View style={styles.bottomItem}>
+                        <Text style={styles.bottomText}>Sat</Text>
+                        <Text style={styles.bottomText}>93</Text>
                     </View>
-                    <View >
-                        <Text style={{ fontFamily: 'CabinetGrotesk-Bold', fontSize: 16 }}>Speed:{Avatardata[0].stats.speed}</Text>
+                    <View style={styles.bottomItem}>
+                        <Text style={styles.bottomText}>Lum</Text>
+                        <Text style={styles.bottomText}>68</Text>
                     </View>
                 </View>
-
-                <View style={{ position: 'absolute', bottom: 0, backgroundColor: 'yellow' }}>
-                    <TouchableOpacity onPress={handleSubmit}>
-                        <Text>Submit</Text>
-                    </TouchableOpacity>
-                </View>
-
-            </ImageBackground>
+            </View>
         </View>
 
     )
 }
+
+const styles = StyleSheet.create({
+
+    card: {
+        display: 'flex',
+        flexDirection:'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 300,
+        width: 250,
+        backgroundColor: '#f9bc61',
+        borderRadius: 8,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 6,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 3,
+        elevation: 10,
+    },
+    content: {
+        display: 'flex',
+        flexDirection: 'column',
+        height: '88%',
+        width: '88%',
+        backgroundColor: 'white',
+        borderRadius: 8,
+    },
+    top: {
+        display: 'flex',
+        flexDirection:'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        height: '20%',
+        backgroundColor: 'white',
+    },
+    topText: {
+        fontSize: 12,
+        fontFamily: 'CabinetGrotesk-Black',
+    },
+    color: {
+        height: 20,
+        width: 20,
+        marginLeft: 5,
+        marginRight: 10,
+        borderRadius: 50,
+        backgroundColor: 'orange',
+    },
+    middle: {
+        display: 'flex',
+        flexDirection:'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        height: '40%',
+        backgroundColor: '#f9bc61',
+        paddingVertical: 15,
+        paddingHorizontal: 15,
+    },
+    middleImage: {
+        width: '60%',
+        height:'100%',
+    },
+    middleText: {
+        marginBottom: 5,
+        fontSize: 12,
+        fontFamily: 'CabinetGrotesk-Black',
+    },
+ 
+    bottom: {
+        display: 'flex',
+        flexDirection:'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        height: '40%',
+        paddingHorizontal: 30,
+    },
+    bottomItem: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    bottomText: {
+        fontFamily: 'CabinetGrotesk-Black',
+    },
+});
+
 
 export default AvatarCard
